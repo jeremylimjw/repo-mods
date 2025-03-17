b3fa43258125085c2d7f56d07aa0e25ae6963cfd
Jeremy Lim <jeremylimjw@gmail.com>
2025-03-18 00:07:17 +0800

    bump

--- sync.bat
+++ sync.bat
@@ -1,8 +1,12 @@
 @echo off
-set "source=.\plugins"
-set "destination=C:\Program Files (x86)\Steam\steamapps\common\REPO\BepInEx\plugins"
+set "source1=.\plugins"
+set "destination1=C:\Program Files (x86)\Steam\steamapps\common\REPO\BepInEx\plugins"
 
-robocopy "%source%" "%destination%" /MIR
+set "source2=.\config"
+set "destination2=C:\Program Files (x86)\Steam\steamapps\common\REPO\BepInEx\config"
+
+robocopy "%source1%" "%destination1%" /MIR
+robocopy "%source2%" "%destination2%" /MIR
 
 echo.
 echo Press any key to continue...
